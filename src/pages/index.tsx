ebbf29afc24221ffad436b0055db11a25953bf4d
Rituraj Pritish <pritishrituraj@gmail.com>
2022-11-13 22:11:23 +0530

    storybook setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { trpc } from '../utils/trpc'
+import { trpc } from '../utils/trpc';
 
 export default function Home() {
   const {data} = trpc.hello.useQuery({ text: 'abcd' })
